284a356d869eec98961e328cfae49c61938bb890
Kavia AI <production@kavia.ai>
2025-12-01 06:36:54 +0000

    Checkpoint 10 - cg541e2969

--- investment_web_frontend/jest.config.js
+++ investment_web_frontend/jest.config.js
@@ -1,16 +1,16 @@
-/** Jest configuration for React app with axios mock mapping. */
+/** Jest configuration for React app with axios mock mapping and Babel transform. */
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^axios$': '<rootDir>/__mocks__/axios.js',
-    // Map CSS and asset imports if present
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    '/node_modules/(?!axios)/',
-  ],
+  // Transpile ESM packages that Jest can't run directly; axios is mocked but included here for safety.
+  transformIgnorePatterns: ['/node_modules/(?!(axios))/'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
 };
--- investment_web_frontend/jest.setup.js
+++ investment_web_frontend/jest.setup.js
@@ -1,2 +1,5 @@
 /* Jest setup for React Testing Library defaults. */
 import '@testing-library/jest-dom';
+
+// Automatically mock axios using __mocks__/axios.js
+jest.mock('axios');
